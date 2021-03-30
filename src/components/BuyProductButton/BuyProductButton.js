080f2f4e934b2279909c5b167f6768b9eb065de9
edjus <ejusto@fi.uba.ar>
2021-03-29 23:05:06 -0300

    create components: BuyProductButton and BackButton

---
+++ src/components/BuyProductButton/BuyProductButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TouchableHighlight, Image, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import styles from './styles';
+
+export default class BuyProductButton extends React.Component {
+  render() {
+    return (
+      <TouchableHighlight underlayColor='rgba(73,182,77,0.9)' onPress={this.props.onPress}>
+        <View style={styles.container}>
+          <Text style={styles.text}>{this.props.title}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+BuyProductButton.propTypes = {
+  onPress: PropTypes.func,
+  title: PropTypes.string
+};
